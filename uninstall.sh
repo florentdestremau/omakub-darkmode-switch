d5578b1fa24ebd945dfc27790e510540ac94a32d
Florent Destremau <florentdestremau@users.noreply.github.com>
2024-07-29 10:58:14 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,5 @@ sudo systemctl stop $SERVICE_NAME
 sudo systemctl disable $SERVICE_NAME
 sudo rm /etc/systemd/system/$SERVICE_NAME
 sudo systemctl daemon-reload
+
+rm $OMAKUB_PATH/bin/dark-mode-switch.sh
